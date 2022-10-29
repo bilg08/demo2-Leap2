927cce6050fb31fc86f3454f7f68af9625cebc8a
Bilguun <zorigoobilguun30@gmail.com>
2022-10-28 21:07:21 -0700

    init

---
+++ frontend/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header, Footer } from '../index';
+import styles from './Layout.module.css'
+export const LayOut = ({children}) => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.mainContainer}>
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </div>
+    );
+}
