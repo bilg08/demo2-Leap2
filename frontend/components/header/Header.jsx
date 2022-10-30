04e1294fb8a813bf7c4f90f3e1240da947f0dce5
Bilguun <zorigoobilguun30@gmail.com>
2022-10-29 20:13:22 -0700

    init mongodb

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 import styles from './Header.module.css'
 export const Header = () => {
     return (
-        <div className={styles.headerContainer}>
-            <div className={styles.logoSection}>
-                <Image width={80} height={50} src={logo} />
-            </div>
-            <ul className={styles.navItems}>
-                <li>Нэвтрэх</li>
-                <li>Бүртгүүлэх</li>
-            </ul>
+      <div className={styles.headerContainer}>
+        <div className={styles.sectionWithLogoAndFindJobs}>
+          <Image width={80} height={50} src={logo} />
+          <ul>
+            <li>Ажил Хайх</li>
+            <li>Эрэлттэй хүмүүс</li>
+          </ul>
         </div>
-    )
+        <ul className={styles.sectionWithLoginAndSignUp}>
+          <li>Нэвтрэх</li>
+          <li>Бүртгүүлэх</li>
+        </ul>
+      </div>
+    );
 }
