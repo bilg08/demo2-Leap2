18682047459dfced5860c586d289be03aee2cebe
Bilguun <zorigoobilguun30@gmail.com>
2022-10-29 20:47:21 -0700

    init mongoose init

---
+++ backend/model/adSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const AdScheme = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Нэрийг оруулна уу"],
+    maxLength: [50, "НЭРНЫ УРТ ИХ БАЙНА"],
+  },
+  photo: {
+    type: String,
+    default: "no-photo.jpeg",
+  },
+  price: {
+    type: Number,
+    required: [true, "Үнийг оруулна уу"],
+    default:"Үнэ тохирно"
+  },
+  detail: {
+    type: String,
+    required: [true, "Дэлгэрэнгүй хэсгийг оруулна уу"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = AdScheme;
