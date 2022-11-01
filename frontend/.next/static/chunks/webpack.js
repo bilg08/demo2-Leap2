9a88e3b861340596454fa63c30b2412caf3f75c9
Bilguun <zorigoobilguun30@gmail.com>
2022-11-01 05:10:38 -0700

    some bug

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e3fe15f9657a7bdc"; }
+/******/ 		__webpack_require__.h = function() { return "1341cd8aecef1df7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
