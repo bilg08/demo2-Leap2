a60d6d65c340e9d990b0fd1edd929297530dc8bc
Bilguun <zorigoobilguun30@gmail.com>
2022-11-02 06:41:11 -0700

    border animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    extend: {},
+  ],
+  theme: {
+    extend: {
+      animation: {
+        border: "border 3s ease",
+      },
+      keyframes: {
+        border: {
+          "0%": { border:'2px solid white' },
+          "100%": { border:'2px solid blue' },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
