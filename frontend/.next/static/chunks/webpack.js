8b8f6544f85c12b1e82b2bc03664f350150f2483
bilguunLeap2 <bilguunleap2@KEKW.local>
2022-11-03 10:10:17 +0800

    changed login

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e38a9493bc65c47c"; }
+/******/ 		__webpack_require__.h = function() { return "43f063d99064544b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
