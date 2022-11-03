19503cd6c22bb0301d9350e2e3a1fc38e296cfe0
Gantulga <86546702+Thelofx123@users.noreply.github.com>
2022-11-03 10:29:30 +0800

    asynchandler

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -2,18 +2,14 @@ const PostSchema = require("../model/postSchema");
 const MyError = require("../utils/myError");
 const asyncHandler = require("../middleware/asyncHandler");
 
-exports.getPosts = async (req, res, next) => {
-  try {
-    const posts = await PostSchema.find();
+exports.getPosts = asyncHandler(async (req, res, next) => {
+  const posts = await PostSchema.find();
 
-    res.status(200).json({
-      success: true,
-      data: posts,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: posts,
+  });
+});
 
 exports.getPost = asyncHandler(async (req, res, next) => {
   const posts = await PostSchema.findById(req.params.id);
@@ -28,50 +24,40 @@ exports.getPost = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.createPost = async (req, res, next) => {
-  try {
-    const newPost = await PostSchema.create(req.body);
+exports.createPost = asyncHandler(async (req, res, next) => {
+  const newPost = await PostSchema.create(req.body);
 
-    res.status(200).json({
-      success: true,
-      data: newPost,
-    });
-  } catch (err) {
-    next(err);
+  res.status(200).json({
+    success: true,
+    data: newPost,
+  });
+});
+
+exports.deletePost = asyncHandler(async (req, res, next) => {
+  const posts = await PostSchema.findByIdAndDelete(req.params.id);
+
+  if (!posts) {
+    throw new MyError("not found", 400);
   }
-};
 
-exports.deletePost = async (req, res, next) => {
-  try {
-    const posts = await PostSchema.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    success: true,
+    data: posts,
+  });
+});
 
-    if (!posts) {
-      throw new MyError("not found", 400);
-    }
-    res.status(200).json({
-      success: true,
-      data: posts,
-    });
-  } catch (err) {
-    next(err);
+exports.updatePost = asyncHandler(async (req, res, next) => {
+  const posts = await PostSchema.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!posts) {
+    throw new MyError("not found", 400);
   }
-};
 
-exports.updatePost = async (req, res, next) => {
-  try {
-    const posts = await PostSchema.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!posts) {
-      throw new MyError("not found", 400);
-    }
-    res.status(200).json({
-      success: true,
-      data: posts,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: posts,
+  });
+});
